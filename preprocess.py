264a9c2693bfd568bd3904f44b9a42c858fb750a
vanh <dangvananh0805@gmail.com>
2025-02-24 16:37:54 +0700

    add text.vietnamese

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,21 @@
 import argparse
-import text
+import text.vietnamese
 from utils import load_filepaths_and_text
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--out_extension", default="cleaned")
-    parser.add_argument("--text_index", default=1, type=int)
+    parser.add_argument("--text_index", default=2, type=int)
     parser.add_argument(
         "--filelists",
         nargs="+",
         default=[
-            "filelists/ljs_audio_text_val_filelist.txt",
-            "filelists/ljs_audio_text_test_filelist.txt",
+            "filelists/multi_filelist_test.txt",
+            "filelists/multi_filelist_train.txt",
+            "filelists/multi_filelist_val.txt"
         ],
     )
-    parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
+    # parser.add_argument("--text_cleaners", nargs="+", default=["english_cleaners2"])
 
     args = parser.parse_args()
 
@@ -23,7 +24,7 @@ if __name__ == "__main__":
         filepaths_and_text = load_filepaths_and_text(filelist)
         for i in range(len(filepaths_and_text)):
             original_text = filepaths_and_text[i][args.text_index]
-            cleaned_text = text._clean_text(original_text, args.text_cleaners)
+            cleaned_text = text._clean_text(original_text)
             filepaths_and_text[i][args.text_index] = cleaned_text
 
         new_filelist = filelist + "." + args.out_extension
---
+++ text/vietnamese.py
@@ -0,0 +1,91 @@
+# from .symbols import symbols
+# from . import cleaners
+# import re
+
+from viphoneme import syms, vi2IPA_split, vi2IPA
+
+symbols = syms
+
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+_id_to_symbol = {i: s for i, s in enumerate(symbols)}
+
+
+
+def sequence_to_text(sequence):
+    
+    result = ''
+    for symbol_id in sequence:
+        if symbol_id in _id_to_symbol:
+            result += _id_to_symbol[symbol_id]     
+    return result
+
+def text_to_sequence(text):
+
+    '''Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+        Args:
+        text: string to convert to a sequence
+        cleaner_names: names of the cleaner functions to run the text through
+        Returns:
+        List of integers corresponding to the symbols in the text
+    '''
+    sequence = []
+    text = text.replace('\s+',' ').lower()
+    phon = vi2IPA_split(text,"/")
+    phon = phon.split("/")[1:]
+    eol = -1
+    for i,p in reversed(list(enumerate(phon))):
+        if p not in ["..",""," ",".","  "]:
+            eol = i
+            break
+
+    phones = phon[:eol+1]+[" ","."]
+    print(phones)
+    phones_id =[]
+    for i in phones:
+        if i in _symbol_to_id:
+            phones_id.append(_symbol_to_id[i])
+            #phones_id = [_symbol_to_id[i] for i in phones]
+    sequence.extend(phones_id)  
+
+    return sequence
+
+
+def cleaned_text_to_sequence(cleaned_text):
+    '''Converts a string of phonems_split with / to a sequence of IDs corresponding to the symbols in the text.
+        Args:
+        text: string of phonems_split to convert to a sequence
+        cleaner_names: names of the cleaner functions to run the text through
+        Returns:
+        List of integers corresponding to the symbols in the text
+    '''
+    sequence = []
+    phon = vi2IPA_split(cleaned_text,"/")
+    phon = phon.split("/")[1:]
+    eol = -1
+    for i,p in reversed(list(enumerate(phon))):
+        if p not in ["..",""," ",".","  "]:
+            eol = i
+            break
+
+    phones = phon[:eol+1]+[" ","."]
+    phones_id =[]
+    for i in phones:
+        if i in _symbol_to_id:
+            phones_id.append(_symbol_to_id[i])
+            #phones_id = [_symbol_to_id[i] for i in phones]
+    sequence.extend(phones_id)
+
+    return sequence
+
+def _clean_text(text):
+    sequence = []
+    text = text.replace('\s+',' ').lower()    
+    phon = vi2IPA(text)
+    print(phon)
+    eol = -1
+    for i,p in reversed(list(enumerate(phon))):
+        if p not in ["..",""," ",".","  "]:
+            eol = i
+            break
+    phones = phon[:eol+1]+" ."
+    return phones
